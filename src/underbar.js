1dfdcebbc17763c90dd81df81ffdd573f9374d84
Roberto Alvarez <robertoatc02@gmail.com>
2014-02-11 22:06:27 -0600

    fixed reject

--- src/underbar.js
+++ src/underbar.js
@@ -103,6 +103,11 @@ var _ = { };
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+
+    return _.filter(collection, function(value){
+
+      return !test(value);
+    });
   };
 
   // Produce a duplicate-free version of the array.
